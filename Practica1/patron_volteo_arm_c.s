56329ff4a1b92c46055c564363e412d9cab11ce8
José Marín Díez <josemarindiez@gmail.com>
2020-09-29 18:56:45 +0200

    modificacion arm_c

--- Practica1/patron_volteo_arm_c.s
+++ Practica1/patron_volteo_arm_c.s
@@ -1,19 +1,18 @@
 
 	PRESERVE8
 	AREA codigo, CODE, READONLY
-	EXPORT patron_volteo_arm
+	EXPORT patron_volteo_arm_c
 	IMPORT ficha_valida
 
-patron_volteo_arm
+patron_volteo_arm_c
 ;r0 = *tablero, r1 = *longitud, r2 = FA, r3 = CA, 
 ;r4 = SF, r5 = SC, r6 = color
-		PUSH {lr}
-        PUSH {fp}
+		PUSH {lr,fp}
         mov fp,sp
 		PUSH {r4-r10}		;guardo los registros que voy a utilizar
-        ldr r4, [fp,#8]
-        ldr r5, [fp, #12]
-        ldr r6, [fp,#16]
+        ldrb r4, [fp, #8]
+        ldrb r5, [fp, #12]
+        ldrb r6, [fp, #16]
 		;recoloco las variables
 		;r1 = FA, r2 = CA, r7 = *longitud 
 		mov r7,r1
@@ -25,10 +24,10 @@ patron_volteo_arm
 		add r2,r2,r5		;CA = CA + SC
 
 		;casilla = r8 = ficha_valida(tablero, FA, CA, &posicion_valida)
-		PUSH{r0-r3}
+		PUSH {r0-r3}
 		bl ficha_valida 
-		mov r0,r8 
-		POP{r0-r3}	
+		mov r8,r0 
+		POP {r0-r3}	
 		
 		ldr r9, [r7] ; r9 =longitud
 while 	ldr r10, [r3] ; r3= posicion_valida
@@ -41,10 +40,10 @@ while 	ldr r10, [r3] ; r3= posicion_valida
 		add r9, r9, #1		; logitud ++
 
 		;casilla = r8 = ficha_valida(tablero, FA, CA, &posicion_valida)
-		PUSH{r0-r3}
+		PUSH {r0-r3}
 		bl ficha_valida 
-		mov r0,r8 
-		POP{r0-r3}
+		mov r8,r0 
+		POP {r0-r3}
 
 		b while
 
@@ -59,8 +58,6 @@ end_while
 		movls r0, #1
 
 _else	str r10, [r7] ; Se guarda la longitud
-		add sp,sp,#4			;reservamos espacio para posicion_valida
-		POP {r7-r10}
-		POP {fp}
-		POP {pc}
+		;add sp,sp,#4			;reservamos espacio para posicion_valida
+		POP {r4-r10,fp,pc}
     END 
