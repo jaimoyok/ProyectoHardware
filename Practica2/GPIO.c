ef79825f7c65f4d7f6931b1e568fe9b5117e74b6
José Marín Díez <josemarindiez@gmail.com>
2020-11-05 16:38:53 +0100

    arreglo GPIO.c

--- Practica2/GPIO.c
+++ Practica2/GPIO.c
@@ -10,44 +10,41 @@ void GPIO_iniciar(void) {
 }
 
 int GPIO_leer(int bitInicial, int numBits) {
-    if(activado){
-         uint32_t aux = IOPIN;
-        aux = aux << bitInicial;
-        return (aux >> (32-numBits)) << numBits;
+    uint32_t aux = -1;
+    if(activado) {
+        aux = IOPIN;
+        aux = aux >> bitInicial; //Se eliminan los bits de la der.
+        aux = aux << (32 - numBits) >> (32 - numBits); //Se eliminan los bits de la izq.
     }
-   
+    return aux;
 }
 
 void GPIO_escribir(int bitInicial, int numBits, int valor) {
     if (activado) {
         int aux = 0;
-        for (int i = 0; i < 32){
-         if (i >= bitInicial && i < bitInicial + numBits)aux += (0x1 << i);
-        }   
+        valor <<= 32 - numBits; //Elimina los bits sobrantes de valor 
+        aux = valor >> (32 - bitInicial - numBits), //Escribe los bits de valor en la posicion dada
         IOSET = aux;
     }
-    
-
 }
 
-//Los bits indicados se utilizaran como bits de salida.
+//Los bits indicados se utilizaran como bits de entrada.
 void GPIO_marcar_entrada(int bitInicial, int numBits) {
-    if(activado0){
-         int aux = 0;
-        for (int i = 0; i < 32){
-            if (i >= bitInicial && i < bitInicial + numBits)aux += (0x1 << i);
-        }   
-    IODIR = aux;
-    }
-   
+    if(activado){
+        int aux = 0;
+        for (int i = bitInicial; i < bitInicial + numBits; ++i) {
+            aux |= (1 << i); //Poner bit a 1
+        }
+        IODIR = aux;
+    } 
 }
 
 //Los bits indicados se utilizaran como bits de salida
 void GPIO_marcar_salida(int bitInicial, int numBits) {
     if(activado){
-         int aux = 0;
-        for (int i = 0; i < 32){
-            if (i < bitInicial && i >= bitInicial + numBits)aux += (0x1 << i);
+        int aux = 0xFFFFFFFF;
+        for (int i = bitInicial; i < bitInicial + numBits; ++i) {
+            aux &= ~(0x1 << i); //Poner bit a 0
         }   
         IODIR = aux;
     }
