4519456e080f2dae3e21ae1323b7cb9b09955f29
jaimoyok <jaimeyoldi2@gmail.com>
2020-11-23 12:48:30 +0100

    12:48

--- Practica2/timer1.c
+++ Practica2/timer1.c
@@ -6,8 +6,10 @@
 #include "Eventos.h"
 #include <stdint.h>
 // variables para contabilizar el n�mero de interrupciones
-static volatile long timer1_int_count = 0;
-
+const int MAX_CONT
+static volatile unsigned long timer1_int_count = 0;
+static volatile int contadores = 0;
+static volatile long inicio_contadores[MAX_CONT];
 void timer1_ISR (void) __irq;    // Generate Interrupt
 
 
@@ -15,6 +17,7 @@ void timer1_ISR (void) __irq;    // Generate Interrupt
 void temporizador1_iniciar (void) {
 		APBDIV |= 1;
 		timer1_int_count = 0;
+
 	  // configuration of Timer 1
     T1MR0 = 599999;                      // Interrumpe cada 0,1ms
     T1MCR = 3;                              // Generates an interrupt and resets the count when the value of MR0 is reached
@@ -39,6 +42,29 @@ unsigned int temporizador1_leer(void){
 
 void temporizador1_parar(void) {
   VICIntEnClr = VICIntEnClr | 0x00000020;
+}
+
+//inicia un contador y devuelve su identificador
+//devuelve menos uno si no se puede iniciar
+int iniciar_contador(){
+	if (contadores == MAX_CONT)return -1;
+	inicio_contadores[contadores] = timer1_int_count;
+	contadores ++;
+	return contadores-1;
+}
+
+//resetea el contador con identificador numContador
+//si este no se ha inicado no hace nada
+void resetear_contador(int numContador){
+	if (numContador > contadores)return;
+	inicio_contadores[numContador] = timer1_int_count;
+}
+
+//devuelve el valor actual del contador con identificador numContador
+//devuelve -1 si este no se ha iniciado
+int contador(int numContador){
+	if (numContador > contadores)return -1;
+	return inicio_contadores[numContador] - timer1_int_count;
 }
 
 /* Timer Counter 0 Interrupt executes each 10ms @ 60 MHz CPU Clock */
