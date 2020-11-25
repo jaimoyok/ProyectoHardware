741e7ef38eb26712670a22e3f7d4bfec171c0575
Jaime Yoldi <jaimeyoldi2@gmail.com>
2020-11-25 20:34:16 +0100

    f

--- Practica2/timer1.h
+++ Practica2/timer1.h
@@ -5,5 +5,8 @@ void temporizador1_iniciar (void);
 void temporizador1_empezar(void);
 unsigned int temporizador1_leer(void);
 unsigned int temporizador1_parar(void);
+int iniciar_contador(void);
+void resetear_contador(int numContador);
+int contador(int numContador);
 
 #endif
