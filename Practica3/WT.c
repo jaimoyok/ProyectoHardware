8060015df211f3ba4304cf973fd817be893771f8
José Marín Díez <josemarindiez@gmail.com>
2020-12-15 16:48:19 +0100

    WT

---
+++ Practica3/WT.c
@@ -0,0 +1,21 @@
+#include <LPC210x.H>                       /* LPC210x definitions */
+#include "Serial_port.h" 
+#include "SWI.h"
+
+void WT_init(int sec){
+	if( WDMOD & 0x04 )						/*El bit 2 se activa si se ha disparado el watchdog*/
+		WDMOD &= ~0x04;						/* Clear time out flag*/
+
+	// Time out: Pclk*WDTC*4
+	// valor m�nimo WTC= 256; valor m�ximo 2^32 (si escribes algo menos que FF, se escribe FF)
+	WDTC  = 60000000  * sec; 					/*60Mhz -> 60M tics por segundo*/	 // Set watchdog time out value
+	WDMOD = 0x03;                            	/* Enable watchdog timer (bit 0) and reset (bit 1).  */  
+	feed_watchdog();			 				/*Se alimenta para comenzar la cuenta*/
+}
+
+void feed_watchdog (void) {				   /* Reload the watchdog timer       */
+  	disable_isr_fiq();	//Se desabilitan las interupciones
+	WDFEED = 0xAA;						   
+ 	WDFEED = 0x55;
+	enable_ise_fiq();	//Se habilitan las interrupciones
+}
---
+++ Practica3/WT.h
@@ -0,0 +1,7 @@
+#ifndef __WT__H
+#define __WT__H
+
+void WT_init(int sec);
+void feed_watchdog (void);
+
+#endif
