37fb16ed5ce27f8f0dc1910c4ddde6224e9f20e7
José Marín Díez <josemarindiez@gmail.com>
2020-12-20 15:54:55 +0100

    Eliminando crimenes de lesa humanidad

--- Practica3/comandos.c
+++ Practica3/comandos.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "comandos.h"
 #include "Eventos.h"
 #include "UART0.h"
@@ -17,27 +18,36 @@ void buscar_comando(char leido) {
     break;
   case FINAL_COMANDO: // Final de un comando '!'
     if (i == 3) {
-      if (comando[0] == 'P' & comando[1] == 'A' & comando[2] == 'S')
+      if (strcmp(comando,"PAS") == 0)
         dato = PASAR;
-      else if (comando[0] == 'N' & comando[1] == 'E' & comando[2] == 'W')
+			
+      else if (strcmp(comando,"NEW") == 0)
         dato = NUEVA_PARTIDA;
-      else if (comando[0] == 'R' & comando[1] == 'S' & comando[2] == 'T')
+			
+      else if (strcmp(comando,"RST") == 0)
         dato = ACABAR_PARTIDA;
+			
       else if (comando[0] >= '0' & comando[1] >= '0' & comando[2] >= '0' &
                comando[0] < '8' & comando[1] < '8' & comando[2] < '8') {
+								 
         if (checksum()) {
           dato = ((comando[1] - '0') << 8) |
                  (comando[0] - '0'); // Conversion a formato de evento
+					
         } else {
           print("Checksum invalido");
           dato = COMANDO_FALLIDO;
         }
+				
       } else
         dato = COMANDO_FALLIDO;
+			
     } else
       dato = COMANDO_FALLIDO;
+		
     cola_guardar_eventos(EV_COMANDO, dato);
     break;
+		
   default: // Letra del comando
     if (i < DIM_COMANDO) {
       comando[i] = leido;
